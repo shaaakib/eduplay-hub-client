547af0ccaa31853f86f239dfdc308733c6c718a1
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-05-18 18:41:31 +0600

    added mytoys row in this projects

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
+import MyToysRow from './MyToysRow';
 
 export default function MyToys() {
   const { user } = useContext(AuthContext);
@@ -15,6 +16,33 @@ export default function MyToys() {
   return (
     <div>
       <h2 className="text-5xl">Your Toys: {myToys.length}</h2>
+
+      {/* table  */}
+
+      <div className="overflow-x-auto">
+        <div className="flex items-center justify-center  font-sans overflow-hidden">
+          <div className="w-full ">
+            <div className="bg-white shadow-md rounded my-6">
+              <table className="min-w-max w-full table-auto">
+                <thead>
+                  <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                    <th className="py-3 px-6 text-left">Project</th>
+                    <th className="py-3 px-6 text-left">Client</th>
+                    <th className="py-3 px-6 text-center">Users</th>
+                    <th className="py-3 px-6 text-center">Status</th>
+                    <th className="py-3 px-6 text-center">Actions</th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-600 text-sm font-light">
+                  {myToys.map((myToy) => (
+                    <MyToysRow key={myToy._id} myToy={myToy} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
