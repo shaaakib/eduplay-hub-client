e932804a4484757636fe2392244a87b47d687640
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-05-19 09:08:46 +0600

    added image gallery & complete design

--- src/Pages/Home/ImageGallery/ImageGallery.jsx
+++ src/Pages/Home/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ImageGallery() {
   return (
     <div>
-      <h1 className="text-center font-bold py-10 text-3xl dark:text-white">
-        Our Toys Gallery
+      <h1 className="text-center font-bold py-10 text-5xl dark:text-white">
+        New Collection
       </h1>
+      <p className="text-center text-lg font-bold">
+        Customers’ favorite products this week. What’s so special?
+        <Link className="text-2xl text-yellow-500"> Check it out!</Link>
+      </p>
 
       <div className="container lg:px-32 px-4 py-8 mx-auto items-center ">
         <div className="grid grid-cols-4 grid-rows-4 grid-flow-col gap-2">
